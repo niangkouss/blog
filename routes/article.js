e9281c15ac0e3067ccc2393ec3f8da0e83562716
niangkouss <takeevernote@163.com>
2017-09-03 14:32:15 +0800

    实现更新文章功能

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,7 @@ let router = express.Router();
 let {checkNotLogin,checkLogin} = require('../auth');
 let {Article} = require('../model');
 router.get('/add',checkLogin,function (req,res) {
-    res.render('article/add',{title:'发表文章'});
+    res.render('article/add',{title:'发表文章',article:{}});
 });
 router.post('/add',checkLogin,function (req,res) {
         let article = req.body;
@@ -30,15 +30,34 @@ router.get('/detail/:_id',function (req,res) {//_id是路径参数
     });
 });
 router.get('/delete/:id',function (req,res) {
-    let _id = req.params._id;
+    let _id = req.params.id;
     Article.remove({_id},function (err,result) {
         if(err){
             req.flash('error',err);
             res.redirect('back');
         }else{
+            console.log(req.params.id);
             req.flash('success','删除文章成功');
             res.redirect('/');
         }
+    });
+});
+router.get('/update/:id',function (req,res) {
+    let _id = req.params.id;
+    Article.findById({_id},function (err,article) {
+        res.render('article/add',{title:'更新文章',article})
+    });
+});
+router.post('/update/:id',function (req,res) {
+    let _id = req.params.id;
+    Article.update({_id},req.body,function (err,result) {
+       if(err){
+           req.flash('error',err);
+           res.redirect('back');
+       }else{
+           req.flash('success','文章更新成功');
+           res.redirect('/article/detail/'+_id)
+       }
     });
 });
 module.exports = router;
